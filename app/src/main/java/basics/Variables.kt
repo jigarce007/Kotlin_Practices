b7e8f78ac52f038dee50a1684b9e62686c0ca050
jigarmakwana <jigarce007>
2022-06-24 23:08:31 +0530

    Variable

---
+++ app/src/main/java/basics/Variables.kt
@@ -0,0 +1,15 @@
+package basics
+
+class Variables {
+
+
+}
+
+fun main() {
+    var name : String = "Jigar"
+    var age = "30"
+    var city = "Ahmedabad"
+    var company = "Maybank Labs"
+
+    println("My name is $name and i am $age\n I am living in $city and working in $company")
+}
